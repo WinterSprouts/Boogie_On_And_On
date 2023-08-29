11e7a490b1119105ec90ec6524d4b0fff8e1603f
J.M.Bae <bejm1997@gmail.com>
2023-08-29 13:30:43 +0900

    init commit

---
+++ src/test/java/wintersprouts/boogie/BoogieApplicationTests.java
@@ -0,0 +1,13 @@
+package wintersprouts.boogie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoogieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
