10a5052eef1b9db5c7f230ce1e34d65ca41b13a5
J.M.Bae <78770745+Dalaranian@users.noreply.github.com>
2023-08-31 14:38:12 +0900

    Resolve issues caused by variable name change (memberId -> email) (#2)
    1. MemberRepository 수정
    2. CustomUserDetailService 수정
    3. MemberService 수정

--- src/main/java/wintersprouts/boogie/service/CustomUserDetailService.java
+++ src/main/java/wintersprouts/boogie/service/CustomUserDetailService.java
@@ -15,14 +15,14 @@ public class CustomUserDetailService implements UserDetailsService {
     private final MemberRepository memberRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserDetails userDetails = memberRepository.findByMemberId(username).map(this::createUserDetails)
+        UserDetails userDetails = memberRepository.findByEmail(username).map(this::createUserDetails)
                 .orElseThrow(() -> new UsernameNotFoundException("해당하는 유저가 없습니다. "));
         return userDetails;
     }
 
     private UserDetails createUserDetails(Member member) {
         Member build = member.builder()
-                .memberId(member.getEmail())
+                .email(member.getEmail())
                 .password(member.getPassword())
                 .role(member.getRole())
                 .build();
