5a9d7ef79b71d6e79722089dd601220b123c622e
J.M.Bae <bejm1997@gmail.com>
2023-08-31 22:58:54 +0900

    Update Donation.java
    Donation Entity 생성

--- src/main/java/wintersprouts/boogie/domain/donation/Donation.java
+++ src/main/java/wintersprouts/boogie/domain/donation/Donation.java
@@ -1,4 +1,71 @@
 package wintersprouts.boogie.domain.donation;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import org.hibernate.annotations.Fetch;
+import wintersprouts.boogie.domain.donator.Donator;
+import wintersprouts.boogie.domain.member.Member;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="DONATION")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@ToString
 public class Donation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "DONATION_ID")
+    private Long donationId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="MEMBER_ID")
+    @Column(name="DONATION_PUBLISHER")
+    private Member donationPublisher;
+
+    @Column(name="DONATION_TITLE")
+    private String title;
+
+    @Column(name="DONATION_CONTENT")
+    private String content;
+
+    @Column(name="DONATION_CURRENT_AMOUNT")
+    private int currnetAmount;
+
+    @Column(name="DONATION_TARGET_AMOUNT")
+    private int targetAmount;
+
+    @Column(name = "DONATION_DUE_DATE")
+    private LocalDate dueDate;
+
+    @Column(name="DONATION_CREATE_AT")
+    private LocalDate CreateAt;
+    @Column(name="DONATION_MODIFIED_AT")
+    private LocalDate modifiedAt;
+
+    @Column(name="DONATION_STATUS")
+    @Enumerated
+    private DonationStatus status;
+
+    @JsonIgnore
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name="DONATOR_PUBLISHER_ID")
+    private List<Donator> donators = new ArrayList<>();
+
+    @Builder
+    public Donation(Member donationPublisher, String title, String content, int currnetAmount, int targetAmount, LocalDate dueDate, LocalDate createAt, LocalDate modifiedAt, DonationStatus status) {
+        this.donationPublisher = donationPublisher;
+        this.title = title;
+        this.content = content;
+        this.currnetAmount = currnetAmount;
+        this.targetAmount = targetAmount;
+        this.dueDate = dueDate;
+        CreateAt = createAt;
+        this.modifiedAt = modifiedAt;
+        this.status = status;
+    }
 }
