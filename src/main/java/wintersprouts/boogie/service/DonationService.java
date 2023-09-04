b85ed5714d4484e7b8b45b21290d3c778713409e
J.M.Bae <bejm1997@gmail.com>
2023-09-04 13:00:11 +0900

    Refactor - Extract Interface
    updateExpiredDonations 인터페이스로 추출

--- src/main/java/wintersprouts/boogie/service/DonationService.java
+++ src/main/java/wintersprouts/boogie/service/DonationService.java
@@ -1,9 +1,17 @@
 package wintersprouts.boogie.service;
 
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.transaction.annotation.Transactional;
 import wintersprouts.boogie.domain.donation.Donation;
 
 public interface DonationService {
     public boolean applyDonation(Donation donation);
 
     String approved(Long donationIndex);
+
+    /**
+     * 매일 자정마다, 마감일이 지난 Donation 확인하여<br>
+     * 상태를 변경해줍니다.
+     */
+    void updateExpiredDonations();
 }
--- src/main/java/wintersprouts/boogie/service/DonationServiceImpl.java
+++ src/main/java/wintersprouts/boogie/service/DonationServiceImpl.java
@@ -1,16 +1,17 @@
 package wintersprouts.boogie.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import wintersprouts.boogie.domain.donation.Donation;
 import wintersprouts.boogie.domain.donation.DonationStatus;
-import wintersprouts.boogie.domain.member.Role;
 import wintersprouts.boogie.repository.DonationRepository;
 
 import java.time.LocalDate;
 
+@Slf4j
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -19,6 +20,7 @@ public class DonationServiceImpl implements DonationService {
     private final DonationRepository donationRepository;
 
     @Override
+    @Transactional
     public boolean applyDonation(Donation donation) {
         Donation save = donationRepository.save(donation);
         return save.getDonationId() != null;
@@ -28,10 +30,11 @@ public class DonationServiceImpl implements DonationService {
     @Transactional
     public String approved(Long donationIndex) {
         Donation donation = donationRepository.findById(donationIndex).get();
-        if(donation.getStatus() == DonationStatus.WAITING){
+        if (donation.getStatus() == DonationStatus.WAITING) {
             donation.setStatus(DonationStatus.APPROVED);
+            donation.setCreatedAt(LocalDate.now());
             return "승인 완료";
-        }else {
+        } else {
             return "이미 처리된 요청입니다. ";
         }
     }
@@ -40,8 +43,11 @@ public class DonationServiceImpl implements DonationService {
      * 매일 자정마다, 마감일이 지난 Donation 확인하여<br>
      * 상태를 변경해줍니다.
      */
+    @Override
     @Scheduled(cron = "0 0 0 * * ?")
-    protected void updateExpiredDonations() {
+    @Transactional
+    public void updateExpiredDonations() {
+        log.info("Execute Update Expired Donation");
         LocalDate now = LocalDate.now();
         donationRepository.updateExpiredDonations(DonationStatus.FINISH, now);
     }
