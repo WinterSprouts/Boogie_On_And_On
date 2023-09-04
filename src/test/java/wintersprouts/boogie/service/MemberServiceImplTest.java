b8ab479b97b98bea55e93049a91c53ce8c598d37
LeeHuSung <mn040820@naver.com>
2023-09-04 14:18:42 +0900

    test2

--- src/test/java/wintersprouts/boogie/service/MemberServiceImplTest.java
+++ src/test/java/wintersprouts/boogie/service/MemberServiceImplTest.java
@@ -1,28 +1,28 @@
-package wintersprouts.boogie.service;
-
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import wintersprouts.boogie.domain.member.Member;
-import wintersprouts.boogie.repository.MemberRepository;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
-class MemberServiceImplTest {
-
-    @Autowired MemberServiceImpl memberService;
-    @Autowired MemberRepository memberRepository;
-
-
-    @Test
-    void test() {
-        List<Member> members = memberRepository.findAll();
-        assertThat(members.size()).isEqualTo(2);
-    }
-
-}
+//package wintersprouts.boogie.service;
+//
+//import org.assertj.core.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import wintersprouts.boogie.domain.member.Member;
+//import wintersprouts.boogie.repository.MemberRepository;
+//
+//import java.util.List;
+//
+//import static org.assertj.core.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootTest
+//class MemberServiceImplTest {
+//
+//    @Autowired MemberServiceImpl memberService;
+//    @Autowired MemberRepository memberRepository;
+//
+//
+//    @Test
+//    void test() {
+//        List<Member> members = memberRepository.findAll();
+//        assertThat(members.size()).isEqualTo(2);
+//    }
+//
+//}
